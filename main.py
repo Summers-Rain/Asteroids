f045467098a630872509565abdc6d008280fb88a
Summers-Rain <Dguilliams99@gmail.com>
2025-04-15 10:53:04 -0500

    Da Black Box is here

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import pygame
+import sys
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        screen.fill((0, 0, 0))
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+    display.flip()
 
 if __name__ == "__main__":
     main()
